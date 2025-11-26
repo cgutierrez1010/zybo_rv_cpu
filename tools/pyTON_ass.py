3f5c53874852c895c9285aed1311e98a1ce8a9c5
cgutierrez1010 <cgutirrez@unm.edu>
2025-11-25 17:24:48 -0700

    updated pyTon

--- tools/pyTON_ass.py
+++ tools/pyTON_ass.py
@@ -11,3 +11,10 @@ def parse_line(line):
     parts = re.split(r'[\s,()]+', line)
     parts = [p for p in parts if p!='']
     return parts
+def assemble_instructions(tokens):
+    op=tokens[0]
+    if op=='addi':
+        rd=reg(tokens[1]);
+                rs1=reg(tokens[2]);
+                    imm=int(tokens[3])
+                    opcode=0b0010011
